978882f7cff0502bb1a085dd537cd9baee0ebf9f
kariSally <sallykarisaf@gmail.com>
2023-12-07 04:22:19 -0800

    custom shell

---
+++ simple_shell.c
@@ -0,0 +1,19 @@
+#include “simple_shell.h”
+
+/**
+ *  main - a function that returns 0
+ *  Return: 0
+ */
+
+int main(void)
+{
+	char cmd[128];
+
+	while (1)
+	{
+		prompt_display();
+		usr_input(cmd, sizeof(cmd));
+		execute_cmd(cmd);
+	}
+	return (0);
+}
